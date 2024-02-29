3f770ec46ea79656a0b1901fe868f8cd09fa15e4
ESC-Liang <bushliang2003@hotmail.com>
2024-02-29 12:21:59 +0800

    create user model

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
-import  Express  from "express";;
+import  Express  from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(
+    () =>{console.log("Connected!!!");
+}).
+catch((err) => {console.log(err);})
+;
+
+
 const app= Express();
 app.listen(3000, () => {
     console.log("server is running on port 3000!!!");
